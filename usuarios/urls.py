d34569c962949f9f5bfb6173f4edde200e85bbf9
Antonio-Felix <antonio980felixmedeiros@gmail.com>
2025-05-12 12:50:50 -0300

    update final

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-# from .views import Deletar
 from .views import adicionar_tarefa_view, apagarTarefaView,apagarTodasAllView, mover_para_cima_view, mover_para_baixo_view
 
 
 urlpatterns = [
-    # path('<int:pk>/deletar/', Deletar.as_view(), name='deletar_tarefa'),
     path('adicionarTarefa/', adicionar_tarefa_view, name='adicionarTarefa'),
     path('apagarTarefa/<int:id>', apagarTarefaView, name='apagarTarefa'),
     path('apagarTarefas/', apagarTodasAllView, name='apagarTarefas'),
--- usuarios/views.py
+++ usuarios/views.py
@@ -35,8 +35,6 @@ def painel_view(request):
     tarefas = Tarefa.objects.filter(usuario=request.user).order_by('ordem')
     return render(request, 'usuarios/painel.html', {'usuario': request.user, 'tarefas': tarefas})
 
-
-@login_required
 @login_required
 def adicionar_tarefa_view(request):
     if request.method == 'POST':
@@ -51,7 +49,6 @@ def adicionar_tarefa_view(request):
         tarefa.save()
         return redirect('painel')
 
-    
 @login_required
 def apagarTarefaView(request, id):
     tarefa = Tarefa.objects.get(id=id)
@@ -88,56 +85,6 @@ def mover_para_baixo_view(request, id):
         proxima.save()
     return redirect('painel')
 
-
 def logout_view(request):
     logout(request)
     return redirect('login')
-
-# class Login(View):
-#     def get(self, request):
-#         form = FormLogin()
-#         return render(request, 'usuarios/login.html', {'form': form})
-
-#     def post(self, request):
-#         if request.method == 'POST':
-#             form = FormLogin(request.POST)
-#             if form.is_valid():
-#                 user = authenticate(
-#                     request,
-#                     username=form.cleaned_data['username'],
-#                     password=form.cleaned_data['password']
-#                 )
-#                 if user:
-#                     login(request, user)
-#                     return redirect('painel')
-#         else:
-#             form = FormLogin()
-#         return render(request, 'usuarios/login.html', {'form': form})
-
-# class Usuario(View):
-#     @login_required
-#     def get(self, request):
-#         pass
-#         # print(request)
-#         # tarefas = Tarefa.objects.filter(usuario=request.user).order_by('-data_criacao')
-#         # return render(request, 'usuarios/painel.html', {
-#         #     'usuario': request.user,
-#         #     'tarefas': tarefas
-#         # })
-
-#     @login_required
-#     def post(self, request):
-#         titulo = request.POST.get('titulo')
-#         if titulo:
-#             Tarefa.objects.create(titulo=titulo, usuario=request.user)
-#             return JsonResponse({'status': 'success'})
-#         return JsonResponse({'status': 'error'}, status=400)
-    
-# class Logout(View):
-#     def get(self, request):
-#         logout(request)
-#         return redirect('login')
-
-# class Deletar(View):
-#     def post(self,request):
-#         pass
